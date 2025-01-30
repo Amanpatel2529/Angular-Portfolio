6d5b05d5fc52c3df4c57410a1da4e73d4afa49d3
Aman Patel <amanpatel.ajp@gmail.com>
2025-01-31 00:25:42 +0530

    update about component bugs

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,6 +9,6 @@ export class AboutComponent {
   jobTitle = 'Angular Developer!';
   aboutUser = " Hi, I'm Aman Patel SDE. I'm a final yr CE student at SIT Lonavala with 1.5 years of experience as a full-stack developer. I'm proficient in JavaScript, Node.js, Express js, Angular, and MongoDB. I'm a results-oriented individual with a strong passion for learning. I'm always looking for new challenges and opportunities to grow my skills ";
   downloadCV = 'Download CV';
-  cvUrl = '../../../assets/images/Aman_Job_CV.pdf';
-  profileUrl ='../../../assets/images/Aman.jpg'
+  cvUrl = 'assets/images/Aman_Job_CV.pdf';
+  profileUrl ='assets/images/Aman.jpg'
 }
